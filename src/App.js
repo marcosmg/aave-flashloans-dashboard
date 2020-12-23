572c87a759dd3d53ae139ffa84060efa9c837643
marcosmg <marcosmartig@gmail.com>
2020-12-23 11:29:18 -0300

    Changed title and favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logoaave.png';
 import './App.css';
 import axios from 'axios';
 import styled from 'styled-components';
-import ReactDOM from 'react-dom'
+
 
 
 const Container = styled.div`
