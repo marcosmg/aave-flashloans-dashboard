1c94e8a44f9352d8c9d5da50b86f3f840f5caf17
marcosmg <marcosmartig@gmail.com>
2020-12-23 16:27:58 -0300

    style neumorphism button

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ const Container = styled.div`
   display: block;
   flex-direction: column;
   justify-content: space-between;
-  margin: 0 auto 100px auto;
+  margin: 0 auto 200px auto;
   width: 60%;
   height: auto;
   max-width:1200px;
   word-break: break-word;
-  border: 2px solid black;
-  border-radius: 50px;
+  box-shadow: (5,5,10,-17,#eee);
+ 
 
   @media (max-width: 1200px) {
     margin: 0 auto;
@@ -23,18 +23,19 @@ const Container = styled.div`
   }
 
   p {
-    font-size: 0.7rem;
+    font-size: 1rem;
     padding: 0px 10px;
   }
 
   li {
     padding: 20px 0;
     list-style: none;
-    border-bottom: 0.5px solid black;
+    box-shadow: 5px 5px 11px -10px #888;
+    
 
   }
   li:hover {
-      box-shadow: 5px 5px 11px -10px #888;
+      box-shadow: 5px 5px 11px -8px #888;
       transform: scale(1.01);
     }
 `
@@ -43,18 +44,28 @@ const Button = styled.button`
   font-size: 1.5rem;
   color: white;
   margin: 40px auto;
-  border: 2px solid white;
-  border-radius: 50px;
-  padding: 20px 50px;
-  background-color: transparent;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  -webkit-tap-highlight-color: transparent;
+  border: none;
+  background-color:transparent;
+ padding: 20px 40px;
+  border-radius:10px;
+  box-shadow: -6px -6px 23px -11px #fdfdfd66,
+              3px -4px 5px -6px #df31ff3d,
+              7px 7px 20px 0px #0002,
+              4px 4px 5px 0px #0001,
+              inset 0px 0px 0px 0px #eebcff73,
+              inset 5px 5px 15px 3px #e6aaff11,
+              inset 2px 5px 29px -26px #e6b5ff99,
+              inset 0px 0px 0px 0px #c689f511;
+  transition:box-shadow 0.6s cubic-bezier(.79,.21,.06,.81);
   font-weight: 900;
   cursor: pointer;
 
-  button:hover {
-    transform: scale(1.1);
-    color: orange;
+  &:hover {
+    transform: scale(0.98);
+   
   }
-  
 `
 
 
@@ -74,24 +85,28 @@ class App extends React.Component {
   fetchFlashloans = async () => {
     try {
 
+      const val = 1;
+
+    const query = {
+      query: `
+      {
+          flashLoans(first: ${val}, orderBy: amount, orderDirection: desc ) {
+              id
+              reserve {
+                  name
+                  symbol
+              }
+              amount
+              target
+              timestamp
+          }
+      }
+      `
+  };
+
     const result = await axios.post(
         'https://api.thegraph.com/subgraphs/name/aave/protocol',
-        {
-            query: `
-            {
-                flashLoans(first: 10, orderBy: amount, orderDirection: desc ) {
-                    id
-                    reserve {
-                        name
-                        symbol
-                    }
-                    amount
-                    target
-                    timestamp
-                }
-            }
-            `
-        }
+        query
     );
     console.log(result.data.data.flashLoans);
 
